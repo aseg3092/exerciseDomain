9e77682aa5579bf0256d53f43bd5b598bafdf465
aseg3092 <gabrielsalazar3092@gmail.com>
2022-01-10 05:09:28 +0000

    mostrando los dominios igual q cm sale en las instrucciones

--- app.js
+++ app.js
@@ -3,13 +3,38 @@ let adj = ['great', 'big' ];
 let noun = ['jogger','racoon'];
 
 
+// for(let i=0;i<pronoun.length;i++)
+// {
+//     for(let j=0;j<adj.length;j++)
+//     {
+//         for(let k=0;k<noun.length;k++)
+//         {
+//             console.log(pronoun[i]+adj[j]+noun[k]+".com");
+//         }
+        
+//     }
+// }
+
+/// igual q cm se muestra en las instrucciones
 for(let i=0;i<pronoun.length;i++)
 {
     for(let j=0;j<adj.length;j++)
     {
         for(let k=0;k<noun.length;k++)
         {
-            console.log(pronoun[i]+adj[j]+noun[k]+".com");
+
+            if( i==0 && j ==1)
+            {
+                console.log(pronoun[j]+adj[i]+noun[k]+".com");
+            }
+            else if(i===1 && j===0)
+            {
+                console.log(pronoun[j]+adj[i]+noun[k]+".com");
+            }
+            else{
+                console.log(pronoun[i]+adj[j]+noun[k]+".com");
+            }
+
         }
         
     }
